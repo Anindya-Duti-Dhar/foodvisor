e57207fbd3767a45f97054ef510f813e47c17afe
Duti <anindyadutidhar@gmail.com>
2017-06-04 23:15:23 +0600

    code for login

--- app/src/main/java/foodvisor/com/foodvisor/AccountFragment.java
+++ app/src/main/java/foodvisor/com/foodvisor/AccountFragment.java
@@ -1,13 +1,35 @@
 package foodvisor.com.foodvisor;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import foodvisor.com.foodvisor.activity.LoginActivity;
+import foodvisor.com.foodvisor.activity.MainActivity;
+import foodvisor.com.foodvisor.utils.PrefManager;
+
+import static foodvisor.com.foodvisor.Home.mDetailsToolbarLayout;
+import static foodvisor.com.foodvisor.Home.mHomeToolbarLayout;
+import static foodvisor.com.foodvisor.Home.toolbar;
 
 public class AccountFragment extends Fragment {
     private static final String TAG = AccountFragment.class.getSimpleName();
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+    Button mLogout;
+
+
     public static AccountFragment newInstance() {
         AccountFragment fragment = new AccountFragment();
         return fragment;
@@ -22,5 +44,56 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.account_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                    getActivity().finish();
+                }
+            }
+        };
+
+        mLogout = (Button) view.findViewById(R.id.btn_logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    //sign out method
+    public void signOut() {
+        auth.signOut();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 }
